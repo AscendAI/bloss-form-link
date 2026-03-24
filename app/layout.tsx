fa425574a9512b89a14e33503f7d17508dceffa0
rahin21 <mdrahinzaman21@gmail.com>
2026-03-24 13:08:36 +0600

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Open_Sans, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const openSans = Open_Sans({
+  variable: "--font-open-sans",
   subsets: ["latin"],
 });
 
@@ -25,9 +23,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn("h-full", "antialiased", openSans.variable, "font-sans", geist.variable)}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-black">{children}</body>
     </html>
   );
 }
---
+++ components/GetStartedFrom.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function GetStartedFrom() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1>Get Started Today</h1>
+      <p className="text-4xl font-bold text-white">Fill in your details and unlock access to our exclusive community.</p>  
+    </div>
+  );
+}
